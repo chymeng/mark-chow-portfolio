b53d6c275136fe1c04d99db275c434eeb929a3c7
Chy Meng <chymeng24@gmail.com>
2018-10-06 17:55:10 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import profilePicture from './assets/mark.jpg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-container">
+        <div className="app">
+          <img
+            src={profilePicture}
+            alt="Mark Chow professional headshot"
+            className="profile-image"
+          />
+          <h1 className="primary-heading">MARK CHOW</h1>
+          <h2 className="secondary-heading">
+            Marketing student studying at RMIT from Mexico{' '}
+            <span role="img">🇲🇽</span> Currently based in Melbourne on a student
+            visa but looking for exciting opportunities
+          </h2>
+          <h3 className="tertiary-heading">
+            Coming soon... for urgent inquiries, please call 0433 883 222
+          </h3>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
