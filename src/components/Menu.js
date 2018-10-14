9506955a0bd805eb31197f7a2c16e445dd64f6ed
Chy Meng <chymeng24@gmail.com>
2018-10-14 13:07:09 +1100

    Change menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { links } from '../constants/constants'
+import { links } from '../helpers/constants'
 
 const Menu = () => {
   return (
